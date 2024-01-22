e104cf0d8dbf13c2edc33629247bb7bd09aaa23c
kevinnassara <nassarakevin@gmail.com>
2024-01-22 02:12:22 +0100

    auth page UI

---
+++ components/Modal.vue
@@ -0,0 +1,16 @@
+<template>
+    <div id="Modal" class="fixed grid place-items-center h-screen z-50 bottom-0 bg-black bg-opacity-50 backdrop-blur-md h-screen w-full">
+        <div class="bg-black w-full max-w-[300px] rounded-lg text-white border border-gray-600 p-2">
+            <button
+                @click="useUserStore.isLogoutOverlay = false"
+                class="flex items-center justify-between bg-black w-full p-3"
+            >
+                <div>Close Modal</div>
+            </button>
+        </div>
+    </div>
+</template>
+
+<script setup>
+
+</script>
